21f62d3bde3255b05672529a18b62513a7051f91
pjcambodia1 <35457778+pjcambodia1@users.noreply.github.com>
2026-05-20 01:52:51 +0500

    Delete api/api directory

--- api/api/ping.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.status(200).json({
-    ok: true,
-    route: "/api/ping",
-    time: new Date().toISOString()
-  });
-}
